f942dd0df8222fa497c9c812063cee6dd13dc35e
andSpt <henrrry52@gmail.com>
2024-02-20 20:58:19 +0300

    database.py add init_redis_pool

--- app/database.py
+++ app/database.py
@@ -1,20 +1,17 @@
-
 from typing import AsyncIterator
 from click import echo
+from redis import Redis, asyncio as redis
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-
 
 
 from app.config import settings
 
 
-
 engine = create_async_engine(settings.db_url, echo=settings.db_echo)
 
-async_session = async_sessionmaker(bind=engine,
-                                   autoflush=False,
-                                   autocommit=False,
-                                   expire_on_commit=False)
+async_session = async_sessionmaker(
+    bind=engine, autoflush=False, autocommit=False, expire_on_commit=False
+)
 
 
 async def get_session() -> AsyncIterator:
@@ -22,5 +19,7 @@ async def get_session() -> AsyncIterator:
         yield session
 
 
-
-
+async def init_redis_pool() -> AsyncIterator[Redis]:
+    session = redis.from_url(url=settings.redis_url, decode_responses=True)
+    yield session
+    await session.close()
