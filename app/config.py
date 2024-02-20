d62fd09fcdbc7cd933deda17a376d55b8e4e47b7
andSpt <henrrry52@gmail.com>
2024-02-20 20:57:44 +0300

    config.py add cache_ttl

--- app/config.py
+++ app/config.py
@@ -2,15 +2,10 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    db_url: str = 'postgresql+asyncpg://postgres:postgres@localhost:5432/ylab' 
+    db_url: str = "postgresql+asyncpg://postgres:postgres@127.0.0.1:6040/ylab"
     db_echo: bool = True
-    redis_url: str = 'redis://127.0.0.1:6040'
+    redis_url: str = "redis://127.0.0.1:6379"
+    cache_ttl: int = 500
 
 
 settings = Settings()
-
-
-
-
-
-
