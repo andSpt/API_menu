b5868e6e8011f109bf2153ee82e771dee6b3d110
andSpt <henrrry52@gmail.com>
2024-02-20 15:55:08 +0300

    Add docker-compose postgres, redis. Change config.py

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     db_url: str = 'postgresql+asyncpg://postgres:postgres@localhost:5432/ylab' 
     db_echo: bool = True
+    redis_url: str = 'redis://127.0.0.1:6040'
 
 
 settings = Settings()
