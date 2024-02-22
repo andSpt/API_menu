c75aa21cf403ce81b0ec70383dfe33de5ba0d374
andSpt <henrrry52@gmail.com>
2024-02-22 19:39:29 +0300

    Refactor name function and response dish.py

--- app/routers/dish.py
+++ app/routers/dish.py
@@ -11,52 +11,65 @@ from app.database import get_session
 from app.services.dish_service import DishService
 
 router = APIRouter(
-    prefix='/api/v1/menus/{menu_id}/submenus/{submenu_id}/dishes',
-    tags=['Dish']
+    prefix="/api/v1/menus/{menu_id}/submenus/{submenu_id}/dishes", tags=["Dish"]
 )
 
-@router.get('/',
-            response_model=list[DishResponse],
-            status_code=status.HTTP_200_OK,
-            summary='Получить список блюд'
-            )
-async def get_all(submenu_id: UUID, dish_service: DishService = Depends()):
-    return await dish_service.get_all(submenu_id=submenu_id)
 
-@router.post('/',
-             response_model=DishResponse,
-             status_code=status.HTTP_201_CREATED,
-             summary='Создать блюдо'
-             )
-async def create_dish(submenu_id: UUID, dish_create: DishCreate, dish_service: DishService = Depends()):
-    return await dish_service.create(submenu_id=submenu_id, dish_create=dish_create)
+@router.get(
+    "/",
+    response_model=list[DishResponse],
+    status_code=status.HTTP_200_OK,
+    summary="Получить список блюд",
+)
+async def get_all_dishes(submenu_id: UUID, dish_service: DishService = Depends()):
+    return await dish_service.get_all_dishes(submenu_id=submenu_id)
 
-@router.get('/{dish_id}',
-            response_model=DishResponse,
-            status_code=status.HTTP_200_OK,
-            summary='Получить блюдо')
-async def get_dish(submenu_id: UUID,
-                   dish_id: UUID,
-                   dish_service: DishService = Depends()
-                   ):
+
+@router.post(
+    "/",
+    response_model=DishResponse,
+    status_code=status.HTTP_201_CREATED,
+    summary="Создать блюдо",
+)
+async def create_dish(
+    submenu_id: UUID, dish_create: DishCreate, dish_service: DishService = Depends()
+):
+    return await dish_service.create_dish(
+        submenu_id=submenu_id, dish_create=dish_create
+    )
+
+
+@router.get(
+    "/{dish_id}",
+    response_model=DishResponse,
+    status_code=status.HTTP_200_OK,
+    summary="Получить блюдо",
+)
+async def get_dish(
+    submenu_id: UUID, dish_id: UUID, dish_service: DishService = Depends()
+):
     return await dish_service.get_dish(submenu_id=submenu_id, dish_id=dish_id)
 
-@router.patch('/{dish_id}',
-              response_model=DishResponse,
-              status_code=status.HTTP_200_OK,
-              summary='Обновить блюдо')
-async def update_dish(submenu_id: UUID,
-                      dish_id: UUID,
-                      dish_update: DishUpdate,
-                      dish_service: DishService = Depends()):
-    return await dish_service.update_dish(submenu_id=submenu_id,
-                                          dish_id=dish_id,
-                                          dish_update=dish_update)
 
-@router.delete('/{dish_id}',
-               status_code=status.HTTP_200_OK,
-               summary='Удалить блюдо')
-async def delete_dish(submenu_id: UUID,
-                      dish_id: UUID,
-                      dish_service: DishService = Depends()) -> JSONResponse:
+@router.patch(
+    "/{dish_id}",
+    response_model=DishResponse,
+    status_code=status.HTTP_200_OK,
+    summary="Обновить блюдо",
+)
+async def update_dish(
+    submenu_id: UUID,
+    dish_id: UUID,
+    dish_update: DishUpdate,
+    dish_service: DishService = Depends(),
+):
+    return await dish_service.update_dish(
+        submenu_id=submenu_id, dish_id=dish_id, dish_update=dish_update
+    )
+
+
+@router.delete("/{dish_id}", status_code=status.HTTP_200_OK, summary="Удалить блюдо")
+async def delete_dish(
+    submenu_id: UUID, dish_id: UUID, dish_service: DishService = Depends()
+) -> JSONResponse:
     return await dish_service.delete_dish(submenu_id=submenu_id, dish_id=dish_id)
