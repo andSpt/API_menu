ea62dd4c9c044c05bfd9211e553412a39512877d
andSpt <henrrry52@gmail.com>
2024-02-22 19:38:54 +0300

    Add dish_cache.py, change dish_service.py. Done cache

---
+++ app/cache/dish_cache.py
@@ -0,0 +1,65 @@
+import json
+from asyncio import sleep
+from time import time
+from uuid import UUID
+
+from fastapi import BackgroundTasks, Depends
+from redis import asyncio as aioredis
+
+from app.config import settings
+from app.database import init_redis_pool
+from app.schemas import DishResponse
+
+
+class DishCache:
+    def __init__(
+        self,
+        redis: aioredis.Redis = Depends(init_redis_pool),
+    ) -> None:
+        self.redis = redis
+        self.all_dishes_key: str = "all_dishes"
+        self.last_cache_update_key: str = "last_cache_update_dishes"
+
+    async def set_dish_to_cache(self, dish_id: UUID, dish_data: DishResponse) -> None:
+        await sleep(4)
+        await self.redis.hset(
+            self.all_dishes_key, str(dish_id), dish_data.model_dump_json()
+        )
+        print(f"!!!!!! Add to cache dish {dish_id}!!!!!")
+        await self.redis.expire(name=self.all_dishes_key, time=settings.cache_ttl)
+
+    async def get_cached_dish(self, dish_id: UUID) -> DishResponse | None:
+        cached_dish: json = await self.redis.hget(self.all_dishes_key, str(dish_id))
+        if cached_dish:
+            print(f"!!!!Submenu from cache {dish_id}!!!")
+            return DishResponse.model_validate_json(cached_dish)
+
+    async def get_all_dishes_from_cache(self) -> list[DishResponse] | None:
+        last_update_time: str = await self.redis.get(self.last_cache_update_key)
+
+        if last_update_time and time() - float(last_update_time) < settings.cache_ttl:
+            all_dishes_in_cache: dict = await self.redis.hgetall(self.all_dishes_key)
+            all_dishes: list[DishResponse] = [
+                DishResponse.model_validate_json(value)
+                for value in all_dishes_in_cache.values()
+            ]
+            print(f"!!!!Dishes from cache {[dish.id for dish in all_dishes]}!!!")
+            return all_dishes
+
+    async def set_all_dishes_to_cache(self, dishes: list[DishResponse]) -> None:
+        await sleep(4)
+        for dish in dishes:
+            await self.redis.hset(
+                self.all_dishes_key, str(dish.id), dish.model_dump_json()
+            )
+        await self.redis.expire(self.all_dishes_key, time=settings.cache_ttl)
+        await self.redis.set(self.last_cache_update_key, time())
+        print(f"!!!!!! Add to cache all dishes !!!!!")
+
+    async def update_dish_in_cache(self, dish_id: UUID, dish_updated: DishResponse):
+        await self.set_dish_to_cache(dish_id=dish_id, dish_data=dish_updated)
+        await self.redis.expire(self.all_dishes_key, time=settings.cache_ttl)
+
+    async def delete_dish_from_cache(self, dish_id: UUID):
+        await self.redis.hdel(self.all_dishes_key, str(dish_id))
+        await self.redis.expire(self.all_dishes_key, time=settings.cache_ttl)
--- app/services/dish_service.py
+++ app/services/dish_service.py
@@ -1,52 +1,89 @@
 from uuid import UUID
 
-from fastapi import Depends
+from fastapi import Depends, BackgroundTasks
 from fastapi.responses import JSONResponse
 
 from app.repositories.dish_repository import DishRepository
 from app.models import Dish
 from app.schemas import DishResponse, DishCreate, DishUpdate
+from app.cache.dish_cache import DishCache
 
 
 class DishService:
     def __init__(
-            self,
-            database_repository: DishRepository = Depends()
+        self,
+        background_tasks: BackgroundTasks,
+        database_repository: DishRepository = Depends(),
+        dish_cache: DishCache = Depends(),
     ) -> None:
         self.database_repository = database_repository
+        self.background_tasks = background_tasks
+        self.dish_cache = dish_cache
 
+    async def get_all_dishes(self, submenu_id: UUID) -> list[DishResponse]:
+        all_dishes_in_cache: list[DishResponse] = (
+            await self.dish_cache.get_all_dishes_from_cache()
+        )
+        if all_dishes_in_cache:
+            return all_dishes_in_cache
 
-    async def get_all(
-            self,
-            submenu_id: UUID
-    ) -> list[Dish]:
-        return await self.database_repository.get_all(submenu_id=submenu_id)
+        all_dishes_in_db: list[DishResponse] = (
+            await self.database_repository.get_all_dishes(submenu_id=submenu_id)
+        )
+        self.background_tasks.add_task(
+            self.dish_cache.set_all_dishes_to_cache, all_dishes_in_db
+        )
+        return all_dishes_in_db
 
-    async def create(self, submenu_id: UUID, dish_create: DishCreate) -> Dish:
-        return await self.database_repository.create_dish(submenu_id=submenu_id, dish_create=dish_create)
+    async def create_dish(
+        self, submenu_id: UUID, dish_create: DishCreate
+    ) -> DishResponse:
+        dish: DishResponse = await self.database_repository.create_dish(
+            submenu_id=submenu_id, dish_create=dish_create
+        )
+        self.background_tasks.add_task(
+            self.dish_cache.set_dish_to_cache, dish_id=dish.id, dish_data=dish
+        )
+        return dish
 
-
-    async def get_dish(
-            self,
-            submenu_id: UUID,
-            dish_id: UUID
-    ) -> Dish:
-        return await self.database_repository.get_dish(
-            submenu_id=submenu_id,
+    async def get_dish(self, submenu_id: UUID, dish_id: UUID) -> DishResponse:
+        cached_dish: DishResponse = await self.dish_cache.get_cached_dish(
             dish_id=dish_id
         )
+        if cached_dish:
+            return cached_dish
 
-    async def update_dish(
-            self,
-            submenu_id: UUID,
-            dish_id: UUID,
-            dish_update: DishUpdate
-    ) -> Dish:
-        return await self.database_repository.update_dish(
-            submenu_id=submenu_id,
-            dish_id=dish_id,
-            dish_update=dish_update
+        dish_in_db = await self.database_repository.get_dish(
+            submenu_id=submenu_id, dish_id=dish_id
         )
 
+        self.background_tasks.add_task(
+            self.dish_cache.set_dish_to_cache, dish_id=dish_id, dish_data=dish_in_db
+        )
+        return dish_in_db
+
+    async def update_dish(
+        self, submenu_id: UUID, dish_id: UUID, dish_update: DishUpdate
+    ) -> DishResponse:
+        dish_updated: DishResponse = await self.database_repository.update_dish(
+            submenu_id=submenu_id, dish_id=dish_id, dish_update=dish_update
+        )
+        self.background_tasks.add_task(
+            self.dish_cache.update_dish_in_cache,
+            dish_id=dish_id,
+            dish_updated=dish_updated,
+        )
+
+        return dish_updated
+
     async def delete_dish(self, submenu_id: UUID, dish_id: UUID) -> JSONResponse:
-        return await self.database_repository.delete_dish(submenu_id=submenu_id, dish_id=dish_id)
+        result = await self.database_repository.delete_dish(
+            submenu_id=submenu_id, dish_id=dish_id
+        )
+
+        if result:
+            self.background_tasks.add_task(
+                self.dish_cache.delete_dish_from_cache, dish_id=dish_id
+            )
+
+        return result
