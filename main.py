cfb355351533920c263f1bdc3ef44ce6df2f65c5
andSpt <henrrry52@gmail.com>
2024-02-24 13:04:46 +0300

    no more create all tables in main.py

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
 from fastapi import FastAPI
 import uvicorn
-from contextlib import asynccontextmanager
 
 from app.routers import menu, dish, submenu
 from app.models import Base
 from app.database import engine
 
 
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    async with engine.begin() as conn:
-         await conn.run_sync(Base.metadata.create_all)
-    yield
-    
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 
 app.include_router(menu.router)
@@ -25,7 +14,5 @@ app.include_router(dish.router)
 app.include_router(submenu.router)
 
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8040)
-
-
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8040)
