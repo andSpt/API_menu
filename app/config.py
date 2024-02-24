cee7078099e891dec7da9bfe753f2566d26b338c
andSpt <henrrry52@gmail.com>
2024-02-24 13:05:53 +0300

    add test_db_url to config.py

--- app/config.py
+++ app/config.py
@@ -6,6 +6,9 @@ class Settings(BaseSettings):
     db_echo: bool = True
     redis_url: str = "redis://127.0.0.1:6379"
     cache_ttl: int = 500
+    test_db_url: str = (
+        "postgresql+asyncpg://postgres_test:postgres@127.0.0.1:6040/ylab_test"
+    )
 
 
 settings = Settings()
