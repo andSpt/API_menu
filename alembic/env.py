c6ba6a8f34d423e654fbaa7cbc3216a7d435ebbb
andSpt <henrrry52@gmail.com>
2024-02-29 20:04:18 +0300

    alembic

--- alembic/env.py
+++ alembic/env.py
@@ -23,7 +23,6 @@ from app.config import settings
 
 target_metadata = Base.metadata
 
-
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
